23f3fd959b1032a0e3b8d4f42b6e29e864057b96
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-15 14:04:42 +0100

    Convert exported function to a factory
    We needed a quick way to make sure Mithril doesn't get included twice

--- dist/n.js
+++ dist/n.js
@@ -1,5 +1,3 @@
-/* global m */
-
 import noop from './tools/noop';
 import toArray from './tools/to-array';
 
@@ -9,33 +7,35 @@ import toArray from './tools/to-array';
  * @param {Attributes} [attributes]
  * @param {Node} [childNode]
  */
-export default function n (selector) {
-    var originalConfig;
-    var restArguments = toArray(arguments).slice(1);
-    var secondArgument = restArguments[1];
+export default function nFactory (m) {
+    return function n (selector) {
+        var originalConfig;
+        var restArguments = toArray(arguments).slice(1);
+        var secondArgument = restArguments[1];
 
-    // Read and clean arguments.
-    var attributes = ( (  typeof secondArgument == 'object'
-                       && !('tag' in secondArgument)
-                       && !('subtree' in secondArgument)
-                       )
-                     ? restArguments.shift()
-                     : {}
-                     );
-    var childNode = restArguments.shift();
+        // Read and clean arguments.
+        var attributes = ( (  typeof secondArgument == 'object'
+                           && !('tag' in secondArgument)
+                           && !('subtree' in secondArgument)
+                           )
+                         ? restArguments.shift()
+                         : {}
+                         );
+        var childNode = restArguments.shift();
 
-    // Add config
-    originalConfig = attributes.config || noop;
-    attributes.config = function (element, isInitialized, context) {
-        if (  !isInitialized
-           && childNode instanceof Node
-           ) {
-            element.appendChild(childNode);
-            }
+        // Add config
+        originalConfig = attributes.config || noop;
+        attributes.config = function (element, isInitialized, context) {
+            if (  !isInitialized
+               && childNode instanceof Node
+               ) {
+                element.appendChild(childNode);
+                }
 
-        return originalConfig(element, isInitialized, context);
+            return originalConfig(element, isInitialized, context);
+            };
+
+        // Trigger mithril
+        return m(selector, attributes);
         };
-
-    // Trigger mithril
-    return m(selector, attributes);
     };
