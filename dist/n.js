ecf780ca39307d6c44db68ec8d506f7dcc0dc89b
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2014-12-22 12:42:01 +0100

    Reformat n.js

--- dist/n.js
+++ dist/n.js
@@ -1,66 +1,63 @@
 import m from 'mithril';
 
-function isDom( x ){
+var isDom = function isDom (x) {
     return x.nodeType > 0;
-}
+    };
 
-function notDom( x ){
-    return !isDom( x );
-}
+var notDom = function notDom (x) {
+    return !isDom(x);
+    };
 
-export default function n( ...input ){
+export default function n (...input) {
     // Wrap any DOM arguments in an array:
     // Avoids Mithril parsing DOM as vdom attrs and makes our logic simpler.
-    var view = m( ...( input.map( x => isDom( x ) ? [ x ] : x ) ) );
+    var view = m(...input.map(x => isDom(x) ? [x] : x));
 
-    var cfg  = view.attrs.config;
+    var cfg = view.attrs.config;
     var kids = view.children;
 
-    var dom  = [];
+    var dom = [];
     var vdom = [];
 
     // Divide children between dom & vdom
-    if( Array.isArray( kids ) ){
-        for ( let x of kids ){
-            ( isDom( x ) ? dom : vdom ).push( x );
+    if (Array.isArray(kids)) for (let x of kids) {
+        (isDom(x) ? dom : vdom).push(x);
         }
-    }
 
-    if( dom.length > 0 ){
-        view.attrs.config = ( el, init, context ) => {
-            if( !init ){
-                dom.map( dom, ( node, index ) => {
-                    let next     = kids.slice( kids.indexOf( node ) ).find( notDom );
-                    let location = el.childNodes[ vdom.indexOf( next ) ] || null;
+    if (dom.length) {
+        view.attrs.config = function config (el, init, context) {
+            if (!init) {
+                dom.map(dom, (node, index) => {
+                    var next = kids.slice(kids.indexOf(node)).find(notDom);
+                    var location = el.childNodes[vdom.indexOf(next)] || null;
 
-                    void function insertNode( node ){
-                        if( node instanceof Array ){
+                    (function insertNode (node) {
+                        if (node instanceof Array) {
                             node = node;
-                        }
-                        else if( node.nodeType === node.DOCUMENT_FRAGMENT_NODE ){
-                            node = Array.from( node.childNodes );
-                        }
+                            }
+                        else if (node.nodeType === node.DOCUMENT_FRAGMENT_NODE) {
+                            node = Array.from(node.childNodes);
+                            }
                         else {
-                            return el.insertBefore( node, location );
-                        }
+                            return el.insertBefore(node, location);
+                            }
 
-                        dom[ index ] = node;
+                        dom[index] = node;
 
-                        for( let subNode of node ){
-                            insertNode( subNode );
-                        }
-                    }( node );
-                } );
-            }
+                        for (let subNode of node){
+                            insertNode(subNode);
+                            }
+                        })(node);
 
-            if( cfg ){
-                return cfg( el, init, context );
-            }
-        };
+                    });
+                }
+
+            if (cfg) return cfg(el, init, context);
+            };
 
         // Make sure only the virtual elements are parsed by m.render.
         view.children = vdom;
-    }
+        }
 
     return view;
-}
+    }
