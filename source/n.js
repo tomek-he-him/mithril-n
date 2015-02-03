94b743a7b368bb6e03f07f4c1f389e256f28bcf0
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-04 00:45:03 +0100

    Rewhitespace – if that's a word
    Conflicts:
    	source/n.js

--- source/n.js
+++ source/n.js
@@ -1,32 +1,35 @@
 import m from 'mithril';
 
-export default function n( ...args ){
-	var view = m( ...args );
-	var cfg  = view.attrs.config;
-	var kids = [];
+export default function n (...args) {
+  var view = m(...args);
+  var cfg = view.attrs.config;
+  var kids = [];
 
-	( function recurse( x ){
-		return x instanceof Array ? x.map( recurse ) : kids.push( x );
-	}( view.children ) );
+  (function recurse (x) {
+    return (x instanceof Array
+      ? x.map(recurse)
+      : kids.push(x)
+      );
+    })(view.children);
 
-	var dom  = [];
-	var vdom = [];
+  var dom = [];
+  var vdom = [];
 
-	for (let x of kids) {
-		(x.nodeType ? dom : vdom).push(x);
-    	}
+  for (let x of kids) {
+    (x.nodeType ? dom : vdom).push(x);
+    }
 
-	if( dom.length ){
-		view.attrs.config = function appendDom( el, ...rest ){
-			for ( let node of dom ){
-				el.insertBefore( node, el.childNodes[ kids.indexOf( node ) ] || null );
-			}
+  if (dom.length) {
+    view.attrs.config = function appendDom (el, ...rest) {
+      for (let node of dom) {
+        el.insertBefore(node, el.childNodes[kids.indexOf(node)] || null);
+        }
 
-			return cfg && cfg( el, ...rest );
-		};
+      return cfg && cfg(el, ...rest);
+      };
 
-		view.children = vdom;
-	}
+    view.children = vdom;
+    }
 
-	return view;
-}
+  return view;
+  }
--- test/index.js
+++ test/index.js
@@ -7,56 +7,57 @@ import n from "../source/n";
 
 
 test("Doesn't break Mithril", function (is) {
-    var noop = function () {};
+  var noop = function () {};
 
-    is.deepEqual
-        ( m("div", {an: "attribute", onclick: noop})
-        , n("div", {an: "attribute", onclick: noop})
-        , "Does the same as Mithril with a vnode,"
-        );
+  is.deepEqual
+    ( m("div", {an: "attribute", onclick: noop})
+    , n("div", {an: "attribute", onclick: noop})
+    , "Does the same as Mithril with a vnode,"
+    );
 
-    is.deepEqual
-        ( m("div", {an: "attribute", onclick: noop}, m("a"), [m(".b"), m("#c")])
-        , n("div", {an: "attribute", onclick: noop}, m("a"), [m(".b"), m("#c")])
-        , "even when the vnode has children."
-        );
+  is.deepEqual
+    ( m("div", {an: "attribute", onclick: noop}, m("a"), [m(".b"), m("#c")])
+    , n("div", {an: "attribute", onclick: noop}, m("a"), [m(".b"), m("#c")])
+    , "even when the vnode has children."
+    );
 
-    var changed = false;
-    m.render(document.body,
-        n(".something", {config: function () { changed = true; }})
-        );
-    is.true
-        ( changed
-        , "Doesn't break an existing config attribute,"
-        );
+  var changed = false;
+  m.render(document.body, n
+    ( ".something"
+    , {config: function () { changed = true; }}
+    ));
+  is.true
+    ( changed
+    , "Doesn't break an existing config attribute,"
+    );
 
-    var changedAgain = false;
-    m.render(document.body,
-        n(".somethingElse", {config: function () { changedAgain = true; }},
-            document.createElement("div")
-            )
-        );
-    is.true
-        ( changedAgain
-        , "even when rendering a node."
-        );
+  var changedAgain = false;
+  m.render(document.body, n
+    ( ".somethingElse"
+    , {config: function () { changedAgain = true; }}
+    , document.createElement("div")
+    ));
+  is.true
+    ( changedAgain
+    , "even when rendering a node."
+    );
 
-    is.end();
-    });
+  is.end();
+  });
 
 
 test.skip("Renders real nodes", function (is) {
-    is.pass("Renders a bunch of child nodes mixed with virtual nodes");
-    is.pass("in the correct order");
-    is.pass("without breaking references.");
-    is.pass("Even when called more than once in a single view.");
-    is.end();
-    });
+  is.pass("Renders a bunch of child nodes mixed with virtual nodes");
+  is.pass("in the correct order");
+  is.pass("without breaking references.");
+  is.pass("Even when called more than once in a single view.");
+  is.end();
+  });
 
 
 test.skip("Updates real nodes", function (is) {
-    is.pass("Reacts to updates on the parent vnode,");
-    is.pass("or on virtual children,");
-    is.pass("still keeping node references.");
-    is.end();
-    });
+  is.pass("Reacts to updates on the parent vnode,");
+  is.pass("or on virtual children,");
+  is.pass("still keeping node references.");
+  is.end();
+  });
