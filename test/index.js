27254223ceb63afa45eab5ded706f9a7495a12a3
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-04 00:06:12 +0100

    Write "Doesn't break Mithril" tests

--- test/index.js
+++ test/index.js
@@ -2,18 +2,39 @@ import "6to5/polyfill";
 
 import test from "tape-catch";
 
+import m from "mithril";
 import n from "../source/n";
 
 
-test("Doesn't break Mithril:", function (is) {
-    is.pass("Does the same as mithril with a vnode,");
-    is.pass("even when the vnode has children.");
-    is.pass("Doesn't break an existing config attribute.");
+test("Doesn't break Mithril", function (is) {
+    var noop = function () {};
+
+    is.deepEqual
+        ( m("div", {an: "attribute", onclick: noop})
+        , n("div", {an: "attribute", onclick: noop})
+        , "Does the same as Mithril with a vnode,"
+        );
+
+    is.deepEqual
+        ( m("div", {an: "attribute", onclick: noop}, m("a"), [m(".b"), m("#c")])
+        , n("div", {an: "attribute", onclick: noop}, m("a"), [m(".b"), m("#c")])
+        , "even when the vnode has children."
+        );
+
+    var changed = false;
+    m.render(document.body,
+        n(".something", {config: function () { changed = true; }})
+        );
+    is.true
+        ( changed
+        , "Doesn't break an existing config attribute."
+        );
+
     is.end();
     });
 
 
-test("Renders real nodes:", function (is) {
+test.skip("Renders real nodes", function (is) {
     is.pass("Renders a bunch of child nodes mixed with virtual nodes");
     is.pass("in the correct order");
     is.pass("without breaking references.");
@@ -22,7 +43,7 @@ test("Renders real nodes:", function (is) {
     });
 
 
-test("Updates real nodes:", function (is) {
+test.skip("Updates real nodes", function (is) {
     is.pass("Reacts to updates on the parent vnode,");
     is.pass("or on virtual children,");
     is.pass("still keeping node references.");
