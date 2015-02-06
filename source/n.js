a1004f4f12e32948fbcaf0f70375323d890a1558
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-06 16:24:23 +0100

    Get rid of array destructuring, as discussed in #2
    Otherwise we'd need to include 6to5/polyfill in the ES5 dist. Look [here][polyfill] for reference.
    
    [polyfill]: https://6to5.org/docs/usage/caveats/#polyfills

--- source/n.js
+++ source/n.js
@@ -1,7 +1,7 @@
 import m from 'mithril';
 
-export default function n (...args) {
-  var view = m(...args);
+export default function n () {
+  var view = m.apply(null, arguments);
   var config = view.attrs.config;
 
   // Divide `view.children` between DOM and vDOM. Save all in kids.
@@ -24,14 +24,14 @@ export default function n (...args) {
     });
 
   if (dom.length) {
-    view.attrs.config = function appendDOM (element, ...rest) {
+    view.attrs.config = function appendDOM (element) {
       for (let node of dom) {
         element.insertBefore(node,
           element.childNodes[kids.indexOf(node)] || null
           );
         }
 
-      return config && config(element, ...rest);
+      return config && config.apply(null, arguments);
       };
 
     view.children = vdom;
