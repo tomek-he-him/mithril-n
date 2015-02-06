5e1a7414288fa9108ff03ddf8f18cee63cdb8cac
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-06 16:38:56 +0100

    Add comments here and there

--- source/n.js
+++ source/n.js
@@ -1,10 +1,11 @@
 import m from 'mithril';
 
 export default function n () {
+  // First, let Mithril process our arguments and generate a `view`.
   var view = m.apply(null, arguments);
   var config = view.attrs.config;
 
-  // Divide `view.children` between DOM and vDOM. Save all in kids.
+  // Divide `view.children` nodes between DOM and vDOM. Save all as a flat array in `kids`.
   var kids = [];
   var dom = [];
   var vdom = [];
@@ -20,22 +21,29 @@ export default function n () {
       }
 
     // - and if it is an array, recurse these steps over each of its items
-    kid.forEach(recurse);  // When TCO comes, we should be ready for it.
+    kid.forEach(recurse);  // (when TCO comes, we should be ready for it)
     });
 
+  // If we have DOM nodes among the `view.children`,
   if (dom.length) {
+    // leave just vDOM nodes among children
+    view.children = vdom;
+
+    // and override the view's `config` attribute with the following function:
     view.attrs.config = function appendDOM (element) {
+      // - add every DOM node to the view's root element – its position is determined by the node's
+      //   original position in `kids`
       for (let node of dom) {
         element.insertBefore(node,
           element.childNodes[kids.indexOf(node)] || null
           );
         }
 
+      // - call the original `config` function
       return config && config.apply(null, arguments);
       };
-
-    view.children = vdom;
     }
 
+  // Return the view, ready for rendering.
   return view;
   }
