87732a185d806d7f25f121f106e3fa5eb5afb755
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-06 15:51:46 +0100

    All kids logic in one recursion

--- source/n.js
+++ source/n.js
@@ -3,21 +3,25 @@ import m from 'mithril';
 export default function n (...args) {
   var view = m(...args);
   var cfg = view.attrs.config;
+
+  // Divide `view.children` between DOM and vDOM. Save all in kids.
   var kids = [];
-
-  (function recurse (x) {
-    return (x instanceof Array
-      ? x.map(recurse)
-      : kids.push(x)
-      );
-    })(view.children);
-
   var dom = [];
   var vdom = [];
+  view.children.forEach(function recurse (kid) {
+    // - get rid of invalid values
+    if (!(kid instanceof Object || typeof kid == "string")) return;
 
-  for (let x of kids) if (typeof x == "object" && x !== null || typeof x == "string") {
-    (x instanceof Node ? dom : vdom).push(x);
-    }
+    // - push the child where it belongs, if it's not an array
+    if (!(kid instanceof Array)) {
+      kids.push(kid);
+      (kid instanceof Node ? dom : vdom).push(kid);
+      return;
+      }
+
+    // - and if it is an array, recurse these steps over each of its items
+    kid.forEach(recurse);  // When TCO comes, we should be ready for it.
+    });
 
   if (dom.length) {
     view.attrs.config = function appendDom (el, ...rest) {
